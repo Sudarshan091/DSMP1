a97cfb37fd82f01e95ee687726e90842fef363b5
Sudarshan091 <choudharysudarshan091@gmail.com>
2026-05-16 18:04:04 +0530

    renamed all the files with proper names of that files

--- WEEK_02/SESSION_07/class.py
+++ WEEK_02/SESSION_07/class.py
@@ -1,1 +1,25 @@
-print('hello')
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # Overloading the print/string behavior
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
+
+    # Overloading the '+' operator
+    def __add__(self, other):
+        # self represents the first object (p1)
+        # other represents the second object (p2)
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return Point(new_x, new_y)
+
+# Creating two Point objects
+p1 = Point(2, 4)
+p2 = Point(5, 1)
+
+# Using the overloaded '+' operator
+p3 = p1 + p2
+
+print(p3)  # Output: Point(7, 5)
